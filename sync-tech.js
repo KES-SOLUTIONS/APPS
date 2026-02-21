900a3fa012852daa129a6d40dad07e07883838c8
KES-SOLUTIONS <kopilots.energie.systeme@gmail.com>
2026-02-21 23:13:09 +0100

    Delete sync-tech.js

--- sync-tech.js
+++
@@ -1,9 +0,0 @@
-/**
- * FICHIER DE SYNCHRONISATION - VERSION LOCALE
- */
-console.log("KES Solutions - Mode local activé");
-
-// Synchronisation désactivée (pas d'API Google)
-function syncFromManager() {
-    console.log("Sync local : OK");
-}
